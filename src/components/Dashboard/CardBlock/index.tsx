874ea9b1128644a1c6588304b9e87f6c6c1c49e4
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-02-02 18:00:55 +0530

    dev: optimization

--- src/components/Dashboard/CardBlock/index.tsx
+++ src/components/Dashboard/CardBlock/index.tsx
@@ -3,16 +3,16 @@ import Card from './Cards';
 
 const CardBlock = () => {
   const cards = [
-    { title: 'Total Course', value: '1345', icon: '/icons/TotalCourse.png' },
-    { title: 'Active Students', value: '23k', icon: '/icons/ActiveStudents.png' },
-    { title: 'Total Revenue', value: '$1234.90', icon: '/icons/TotalRevenue.png' },
-    { title: 'New Enrollment', value: '34', icon: '/icons/NewEnrollment.png' },
+    { id: 1, title: 'Total Course', value: '1345', icon: '/icons/TotalCourse.png' },
+    { id: 2, title: 'Active Students', value: '23k', icon: '/icons/ActiveStudents.png' },
+    { id: 3, title: 'Total Revenue', value: '$1234.90', icon: '/icons/TotalRevenue.png' },
+    { id: 4, title: 'New Enrollment', value: '34', icon: '/icons/NewEnrollment.png' },
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-      {cards.map((card, index) => (
-        <Card key={index} title={card.title} value={card.value} icon={card.icon} />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-4 p-4">
+      {cards.map(({ id, title, value, icon }) => (
+        <Card key={id} title={title} value={value} icon={icon} />
       ))}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ import TopCourses from "@/components/Dashboard/TopCourses";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-between m-2  lg:m-8 lg:mx-20 space-y-4 md:space-y-0">
-        <div className="flex flex-col w-full md:w-2/3">
+      <div className="flex flex-col xl:flex-row justify-between m-2  lg:m-8 lg:mx-20 space-y-4 md:space-y-0">
+      
+      
+        <div className="flex flex-col w-full xl:w-2/3">
           <CardBlock />
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
             <RecentActivity />
@@ -16,7 +18,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-full md:w-1/3 px-10">
+        <div className="w-full xl:w-1/3 px-10 md:p-10">
           <RecentActivityTable />
         </div>
       </div>
